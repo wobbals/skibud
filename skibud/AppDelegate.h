abda516952669d56f9c3d2cb940bbe223d3eab74
Charley Robinson <charley@tokbox.com>
2016-01-16 15:23:59 -0700

    Initial Commit

---
+++ skibud/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  skibud
+//
+//  Created by Charley Robinson on 1/16/16.
+//  Copyright © 2016 Wobbbals. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ skibud/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  skibud
+//
+//  Created by Charley Robinson on 1/16/16.
+//  Copyright © 2016 Wobbbals. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
